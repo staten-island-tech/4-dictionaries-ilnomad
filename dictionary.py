b55478ef9a7d08d5bb47fef4e4c23b06f867f8e2
ilnomad <nathanc124@nycstudents.net>
2025-10-03 09:11:31 -0400

    dgf

--- dictionary.py
+++ dictionary.py
@@ -1,24 +1,24 @@
-steamshop={
-    __name__
-    itemID1={
+steamshop = {
+    "itemID1":{
         "name":"Hollow Knight",
         "price":"$14.99",
         "description":"Forge your own path in Hollow Knight! An epic action adventure through a vast ruined kingdom of insects and heroes. Explore twisting caverns, battle tainted creatures and befriend bizarre bugs, all in a classic, hand-drawn 2D style.",
     },
-    itemID2={
+    "itemID2":{
         "name":"Hollow Knight: Silksong",
         "price":"$19.99",
         "description":"Discover a vast, haunted kingdom in Hollow Knight: Silksong! Explore, fight and survive as you ascend to the peak of a land ruled by silk and song.",
     },
-    itemID3={
+    "itemID3":{
         "name":"Undertale",
         "price":"$9.99",
         "description":"UNDERTALE! The RPG game where you don't have to destroy anyone.",
     },
-    itemID4={
-        "name":"Deltarune",
-        "price":"$24.99",
-        "description":"Dive into the parallel story to UNDERTALE! Fight or spare your way through action-packed battles as you explore a mysterious world alongside an endearing cast of new and familiar characters. Chapters 1-4 will be available on launch, with more planned as free updates!",
-    },
+    "itemID4":{
+    "name":"Deltarune",
+    "price":"$24.99",
+    "description":"Dive into the parallel story to UNDERTALE! Fight or spare your way through action-packed battles as you explore a mysterious world alongside an endearing cast of new and familiar characters. Chapters 1-4 will be available on launch, with more planned as free updates!",
+    }
 }
 
+print(steamshop["itemID1"]["name"])
