b7ab2004c14721ee962f8e4dbf92e4969b666e0c
ilnomad <nathanc124@nycstudents.net>
2025-10-08 09:25:32 -0400

    lkj  j lkjljl o u89yo9h89i tgu hgkujguyu guyvhjjgfuybhn   6u gb8uyhjbytghv bnju7y6trfcv bnmujicgf bvnmkji8u bnmokiujhbj nm,lkoiuhjnm ,

--- dictionary.py
+++ dictionary.py
@@ -1,31 +1,49 @@
 steamshop={
-    "Hollow Knight":{
-        "gameID":0,
+    "ID0":{
+        "name":"Hollow Knight",
         "Price":14.99,
         "Description":"Forge your own path in Hollow Knight! An epic action adventure through a vast ruined kingdom of insects and heroes. Explore twisting caverns, battle tainted creatures and befriend bizarre bugs, all in a classic, hand-drawn 2D style.",
     },
-    "Hollow Knight: Silksong":{
-        "gameID":1,
+    "ID1":{
+        "name":"Hollow Knight: Silksong",
         "Price":19.99,
         "Description":"Discover a vast, haunted kingdom in Hollow Knight: Silksong! Explore, fight and survive as you ascend to the peak of a land ruled by silk and song.",
     },
-    "Undertale":{
-        "gameID":2,
+    "ID2":{
+        "name":"Undertale",
         "Price":9.99,
         "Description":"UNDERTALE! The RPG game where you don't have to destroy anyone.",
     },
-    "Deltarune":{
-        "gameID":3,
+    "ID3":{
+        "name":"Deltarune",
         "Price":24.99,
         "Description":"Dive into the parallel story to UNDERTALE! Fight or spare your way through action-packed battles as you explore a mysterious world alongside an endearing cast of new and familiar characters. Chapters 1-4 will be available on launch, with more planned as free updates!",
     }
 }
 
 def buysteamshop():
-    print("What game would you like to buy?")
-    x=input()
-    print("What information would you like about the game? (Price or Description)")
-    y=input()
-    print(steamshop[x][y])
+    cart=[]
+    ContinueShopping=True
+    x=input("Would you like to buy a game? ")
+    while x==("Yes."):
+        while ContinueShopping==True:
+            for h in steamshop:
+                print(steamshop[h]["name"])
+            b=str(input("What game would you like to buy? "))
+            cart.append(b)
+            y=input("Would you like to buy another game? ")
+            if y=="Yes.":
+                ContinueShopping=True
+            else:
+                x=("No.")
+                ContinueShopping=False
+    else:
+        z=input("Would you like to view your cart or continue shopping? ")
+        if z==("Continue Shopping"):
+            ContinueShopping=True
+        else:
+            print(cart)
+            for i in cart:
+                print(cart[i]["price"])
 
 buysteamshop()
--- occupiedspaces.py
+++ occupiedspaces.py
@@ -3,8 +3,9 @@ def occupiedspaces():
     yesterday=list(input("Occupied spaces yesterday? "))
     today=list(input("Occupied spaces today? "))
     a=0
+    print(n,yesterday,today)
     for i in range(n):
-        if yesterday[i]==today[i]:
+        if yesterday[i]==today[i]=="C":
             a=a+1
     print(a)
 
